13b4dfd47a7dc33cb9aac538f5fce4c50c7a968c
MEGAMINDSPARK <rahulkumarroy399@gmail.com>
2023-09-05 19:22:13 +0530

    Delete button

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -3,15 +3,28 @@ import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+
+  const clickHandler = ()=>{
+    console.log("Clicked!!!!");
+  }
+  const deleteHandler = (e)=>{
+    console.log("Deleted!!!!");
+    e.target.parentNode.remove();
+  }
   return (
     <Card className='expense-item'>
-    {/* here Card is custom component defined by me so i have to add class name incard.js  also ,  all the default html components out of the box support class name for adding css classes to render html elements but ur custom components only supports what u tell them to supportsso u have to set className on ur card component */}
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
+    {/* <button onClick= {()=>{console.log('clicked!')}} >Change Title</button> */}
+    <button onClick= {clickHandler}>Change Title</button>
+    
+    <button onClick= {deleteHandler}>Delete</button>
+    {/* i want to change props.title after cliking on button using props: on se start hota hai  */}
     </Card>
+
   );
 }
 
--- src/components/UI/Card.js
+++ src/components/UI/Card.js
@@ -1,11 +1,9 @@
-import React from 'react';
-
-import './Card.css';
-
-const Card = (props) => {
-  const classes = 'card ' + props.className;
-
+import "./Card.css";
+const Card=(props)=> {
+  // props that every components receives(built in props) even if u r never setting it explicitely  is props.children children is the reserve name, we don't set children prop in this class  ,and the value of this special props children will always be the content between opening and the closing tag of our custom component
+  // jo v expenseitem.js ke <Card /> k bich me hai wo props.children hai or iska value hai
+  const classes = "card " + props.className; //here card is default class
+  // return <div className="card">{props.children}</div>
   return <div className={classes}>{props.children}</div>;
-};
-
+}
 export default Card;
