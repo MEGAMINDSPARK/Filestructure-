d818c5e1c1707caf82e0f7ba455a7c0cde787195
MEGAMINDSPARK <rahulkumarroy399@gmail.com>
2023-09-05 20:19:28 +0530

    New form

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,62 +1,92 @@
-import React,{useState} from "react";
+// import React,{useState} from "react";
+// import "./ExpenseItem.css";
+// import Card from "../UI/Card";
+// import ExpenseDate from "./ExpenseDate";
+
+// import "./ExpenseItem.css";
+// const ExpenseItem = (props) => {
+
+//   const  [title,setTitle] = useState(props.title);
+
+//   const [amount,amFun] = useState(props.amount);
+
+
+//   const clickHandler = () => {
+    
+//     setTitle('New Title...')
+  
+//   }
+//   const amountFunc = ()=> {
+//     amFun('100$')
+//   }
+//   return (
+//     <Card className="expense-item">
+// function ExpenseItem(props) {
+
+// const clickHandler = ()=>{
+//     console.log("Clicked!!!!");
+//   }
+//   const deleteHandler = (e)=>{
+//     console.log("Deleted!!!!");
+//     e.target.parentNode.remove();
+//   }
+//   return (
+//     <Card className='expense-item'>
+//       <ExpenseDate date={props.date} />
+//       <div className="expense-item__description">
+//         <h2>{title}</h2>
+//         <div className="expense-item__price">${amount}</div>
+//       </div>
+
+   
+//     <button onClick= {clickHandler}>Change Title</button>
+    
+//     <button onClick= {deleteHandler}>Delete</button>
+//     </Card>
+
+//   );
+// }
+
+//       <button onClick={amountFunc}>Change Expense</button>
+//       <button onClick={clickHandler}>Change Title</button> </Card>
+//   );
+// };
+// export default ExpenseItem;
+
+import React, { useState } from "react";
 import "./ExpenseItem.css";
 import Card from "../UI/Card";
 import ExpenseDate from "./ExpenseDate";
 
-import "./ExpenseItem.css";
 const ExpenseItem = (props) => {
-
-  const  [title,setTitle] = useState(props.title);
-
-  const [amount,amFun] = useState(props.amount);
-
+  const [title, setTitle] = useState(props.title);
+  const [amount, amFun] = useState(props.amount);
 
   const clickHandler = () => {
-    
-    setTitle('New Title...')
-  
-  }
-  const amountFunc = ()=> {
-    amFun('100$')
+    setTitle('New Title...');
   }
 
-<<<<<<< HEAD
-  return (
-    <Card className="expense-item">
-=======
-function ExpenseItem(props) {
-
-  const clickHandler = ()=>{
-    console.log("Clicked!!!!");
+  const amountFunc = () => {
+    amFun('100$');
   }
-  const deleteHandler = (e)=>{
+
+  const deleteHandler = (e) => {
     console.log("Deleted!!!!");
     e.target.parentNode.remove();
   }
+
   return (
     <Card className='expense-item'>
->>>>>>> 13b4dfd47a7dc33cb9aac538f5fce4c50c7a968c
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">${amount}</div>
       </div>
-<<<<<<< HEAD
-=======
-    {/* <button onClick= {()=>{console.log('clicked!')}} >Change Title</button> */}
-    <button onClick= {clickHandler}>Change Title</button>
-    
-    <button onClick= {deleteHandler}>Delete</button>
-    {/* i want to change props.title after cliking on button using props: on se start hota hai  */}
-    </Card>
-
-  );
-}
->>>>>>> 13b4dfd47a7dc33cb9aac538f5fce4c50c7a968c
-
+      <button onClick={clickHandler}>Change Title</button>
+      <button onClick={deleteHandler}>Delete</button>
       <button onClick={amountFunc}>Change Expense</button>
-
-      <button onClick={clickHandler}>Change Title</button> </Card>
+    </Card>
   );
 };
+
 export default ExpenseItem;
